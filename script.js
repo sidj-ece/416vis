fba2b666c761106ebaf3302fcda081a72aa1b940
sidj-ece <sajain4@illinois.edu>
2026-08-01 15:59:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function drawScene1(svg, data) {
   d3.select("#scene-description").text("Vaccine rollouts began slowly in early 2021.");
   const filteredData = data.filter(d => d.entity === "World" && d.date >= new Date("2021-01-01") && d.date <= new Date("2021-06-30"));
 
-  const margin = {top: 10, right: 10, bottom: 10, left: 10};
+  const margin = {top: 50, right: 50, bottom: 50, left: 50};
   const width = +svg.attr("width") - margin.left - margin.right;
   const height = +svg.attr("height") - margin.top - margin.bottom;
   const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
@@ -45,7 +45,7 @@ function drawScene1(svg, data) {
   const x = d3.scaleTime().domain(d3.extent(filteredData, d => d.date)).range([0, width]);
   const y = d3.scaleLinear().domain([0, d3.max(filteredData, d => d.doses_per_hundred) || 100]).range([height, 0]);
 
-  g.append("g").call(d3.axisBottom(x));
+  g.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x));
   g.append("g").call(d3.axisLeft(y));
 
   const line = d3.line().x(d => x(d.date)).y(d => y(d.doses_per_hundred));
